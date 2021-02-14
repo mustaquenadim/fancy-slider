d72660cc9a01f31c696c63eb6caa87ca99f89598
Mustaque Nadim <mustaquenadim2000@gmail.com>
2021-02-14 20:39:48 +0600

    details

--- app.js
+++ app.js
@@ -26,7 +26,14 @@ const showImages = (images) => {
     images.forEach((image) => {
         let div = document.createElement('div');
         div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
-        div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
+        div.innerHTML = `
+            <img 
+                class="img-fluid img-thumbnail img-with-details" onclick=selectItem(event,"${image.webformatURL}") 
+                src="${image.webformatURL}" 
+                alt="${image.tags}"
+            >
+            <div class="top-right"><i class="fas fa-eye"></i> ${image.views}</div>
+        `;
         gallery.appendChild(div);
     });
 };
